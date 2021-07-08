5d45690c8c633ddb1441f8b6b8c597adc368e22c
colinsteidtmann <colinsteidtmann@yahoo.com>
2021-07-08 10:57:53 -0700

    firebase anaylitics

--- src/app/App.js
+++ src/app/App.js
@@ -5,7 +5,22 @@ import { Header, Footer } from "#components";
 import ApiToastUpdate from "./ApiToastUpdate";
 // Pages
 import { Home, Bikes, BikeDetails, Help, NewListing, Terms } from "#pages";
+// Firebase
+import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app"
+const firebaseConfig = {
+  apiKey: "AIzaSyD62-uzn0C85cFy42nXA1L5F3CZ-72AqL8",
+  authDomain: "polycycles-26c39.firebaseapp.com",
+  projectId: "polycycles-26c39",
+  storageBucket: "polycycles-26c39.appspot.com",
+  messagingSenderId: "176931862001",
+  appId: "1:176931862001:web:b6c1db99cc074b94208050",
+  measurementId: "G-EDVKZN8REH"
+};
 
+const firebaseApp = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics();
 
 const App = () => {
   return (
